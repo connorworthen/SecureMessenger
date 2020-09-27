ca9f424751b3d40a041cfba35dbe33473470b81f
connorworthen <connorw220@protonmail.com>
2020-09-27 16:45:27 -0700

    WebSocket setup

--- SecureMessenger_back_end/app/controllers/messages_controller.rb
+++ SecureMessenger_back_end/app/controllers/messages_controller.rb
@@ -3,7 +3,9 @@ class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     if message.save
-      render json: message
+      forum = Forum.find(message.forum_id)
+      ForumChannel.broadcast_to(forum, message)
+      # render json: message
     else
       render json: {errors: game.errors.full_messages}, status: 422
     end
--- SecureMessenger_back_end/config/routes.rb
+++ SecureMessenger_back_end/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :forums, only: [:create, :index, :show]
   resources :messages, only: [:create]
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
