b9c0603d8e41ceba9e7aa490b4080f5380c7bbcc
connorworthen <connorw220@protonmail.com>
2020-09-27 15:57:36 -0700

    message controller methods

--- SecureMessenger_back_end/app/controllers/messages_controller.rb
+++ SecureMessenger_back_end/app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+  
+  def create
+    message = Message.new(message_params)
+    if message.save
+      render json: message
+    else
+      render json: {errors: game.errors.full_messages}, status: 422
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :forum_id)
+  end
+  
 end
