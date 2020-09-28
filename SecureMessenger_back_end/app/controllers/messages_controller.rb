04c394816590c9c2846583bc0f79f45e3e1760f6
connorworthen <connorw220@protonmail.com>
2020-09-27 17:50:54 -0700

    changed to ajax instead of websock

--- SecureMessenger_back_end/app/controllers/messages_controller.rb
+++ SecureMessenger_back_end/app/controllers/messages_controller.rb
@@ -3,9 +3,7 @@ class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     if message.save
-      forum = Forum.find(message.forum_id)
-      ForumChannel.broadcast_to(forum, message)
-      # render json: message
+      render json: message
     else
       render json: {errors: game.errors.full_messages}, status: 422
     end
